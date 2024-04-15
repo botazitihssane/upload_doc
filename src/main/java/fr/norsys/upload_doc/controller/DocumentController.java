e3ae08aef602397ad5d41f82b545ee0c83509b85
botazitihssane <botazitihssane@outlook.com>
2024-04-15 16:11:17 +0100

    added exception to the get method

--- src/main/java/fr/norsys/upload_doc/controller/DocumentController.java
+++ src/main/java/fr/norsys/upload_doc/controller/DocumentController.java
@@ -3,12 +3,14 @@ package fr.norsys.upload_doc.controller;
 import fr.norsys.upload_doc.dto.DocumentDetailsResponse;
 import fr.norsys.upload_doc.service.DocumentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
 @RestController
@@ -20,12 +22,13 @@ public class DocumentController {
 
     @GetMapping("/{id}")
     public ResponseEntity<DocumentDetailsResponse> getDocumentByUUID(@PathVariable UUID id) {
-        DocumentDetailsResponse response = documentService.getDocumentByID(id);
-
-        if (response != null) {
+        try {
+            DocumentDetailsResponse response = documentService.getDocumentByID(id);
             return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.noContent().build();
+        } catch (NoSuchElementException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 }

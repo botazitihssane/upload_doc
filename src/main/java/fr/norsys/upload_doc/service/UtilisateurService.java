74e58eb589269c49bcde21679b88d97d5bbb939a
Oumaima EL MARDI <82413301+oumaimaelmardi@users.noreply.github.com>
2024-04-17 23:04:12 +0000

    save findByid deleteById

--- src/main/java/fr/norsys/upload_doc/service/UtilisateurService.java
+++ src/main/java/fr/norsys/upload_doc/service/UtilisateurService.java
@@ -1,15 +1,16 @@
 package fr.norsys.upload_doc.service;
 
-import fr.norsys.upload_doc.dto.DocumentDetailsResponse;
-import fr.norsys.upload_doc.entity.Document;
-import fr.norsys.upload_doc.entity.Metadata;
 import fr.norsys.upload_doc.entity.Utilisateur;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Set;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UtilisateurService {
+    ResponseEntity<?> save(Utilisateur utilisateur);
+
+    Optional<Utilisateur> findById(UUID id);
+
+    void deleteById(UUID id);
 
 }
--- src/main/java/fr/norsys/upload_doc/service/impl/UtilisateurServiceImpl.java
+++ src/main/java/fr/norsys/upload_doc/service/impl/UtilisateurServiceImpl.java
@@ -1,14 +1,39 @@
 package fr.norsys.upload_doc.service.impl;
 
 import fr.norsys.upload_doc.entity.Utilisateur;
+import fr.norsys.upload_doc.repository.UtilisateurRepository;
 import fr.norsys.upload_doc.service.UtilisateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 public class UtilisateurServiceImpl implements UtilisateurService {
+   @Autowired
+    public UtilisateurRepository utilisateurRepository;
 
+    public ResponseEntity<?> save(Utilisateur utilisateur) {
+        try {
+            Utilisateur savedUtilisateur = utilisateurRepository.save(utilisateur);
+            return ResponseEntity.ok(savedUtilisateur);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred while saving the utilisateur.");
+        }
+    }
 
+    @Override
+    public Optional<Utilisateur> findById(UUID id) {
+        return utilisateurRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+  utilisateurRepository.deleteById(id);
+    }
 }
